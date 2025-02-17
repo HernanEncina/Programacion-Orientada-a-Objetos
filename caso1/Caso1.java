bd56c9a899634e82d89d7cc3fec3242a6a9d8435
Bruh Bruh <hernanhem06@gmail.com>
2025-02-17 07:57:18 -0600

    Caso 1

---
+++ caso1/Caso1.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.caso1;
+
+/**
+ *
+ * @author heckl
+ */
+
+class vehiculo{
+    String marca;
+    String modelo;
+    String año;
+    
+    public void arrancar(){
+        
+    }
+    
+    public void detener(){
+        
+    }
+    
+    class Automovil extends vehiculo{
+        String tipoCombustible;
+        public void abrirPuertas(){
+        
+        }
+        
+    }
+    
+    class Motocicleta extends vehiculo{
+        String hacerCaballitos;
+        public void hacerCaballitos(){
+        
+    }
+    }
+    
+    class Camion extends vehiculo{
+        String cargarMercancia;
+        public void cargarMercancia(){
+            
+        }
+    }
+}
+public class Caso1 {
+
+    public static void main(String[] args) {
+    }
+}
